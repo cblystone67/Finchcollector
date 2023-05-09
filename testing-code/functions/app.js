adadbf6d082a14a5ee0514a4ce7d026ae31b4597
cblystone67 <christopherblystone1@gmail.com>
2023-05-09 08:54:19 -0700

    Set Up

--- testing-code/functions/app.js
+++ testing-code/functions/app.js
@@ -30,3 +30,17 @@ function findIndividual(person){
 }
 console.log((findIndividual('mariam')));
 
+//an array object.
+// const nums = [1,2,3,4,5];
+// for(let n of nums){
+//     console.log(n);
+// }
+const person = {name: 'janice', location: 'ca'}; // <= object literal
+for(let value of person){
+    console.log(value);
+}
+//for ... in loop
+//loops over keys in an object
+// for(let key in nums){
+//     console.log(key);
+// }
