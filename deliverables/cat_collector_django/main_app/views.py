605649869e4f997b5316fd06b9f9ca218a8f32a6
cblystone67 <christopherblystone1@gmail.com>
2023-05-30 18:21:38 -0700

    Stage 1

---
+++ deliverables/cat_collector_django/main_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+
+def cats_index(request):
+  return render(request, 'cats/index.html', {'cats': cats})
+
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
