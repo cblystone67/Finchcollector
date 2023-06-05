76689bbea17cbb38e654103f2ea3bf37ace0c93f
cblystone67 <christopherblystone1@gmail.com>
2023-06-05 13:49:14 -0700

    Finished Protedtion

--- deliverables/cat_collector_django/main_app/views.py
+++ deliverables/cat_collector_django/main_app/views.py
@@ -1,8 +1,12 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cat, Toy
 from .forms import FeedingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -13,10 +17,12 @@ def about(request):
   return render(request, 'about.html')
 
 
+@login_required
 def cats_index(request):
-  cats = Cat.objects.all()
+  cats = Cat.objects.filter(user=request.user)
   return render(request, 'cats/index.html', {'cats': cats})
 
+@login_required
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   id_list = cat.toys.all().values_list('id') #[1, 3, 7]
@@ -25,6 +31,7 @@ def cats_detail(request, cat_id):
   return render(request, 'cats/detail.html', {
     'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have})
   
+@login_required
 def add_feeding(request, cat_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -33,47 +40,63 @@ def add_feeding(request, cat_id):
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
 
+@login_required
 def assoc_toy(request, cat_id, toy_id):
   Cat.objects.get(id=cat_id).toys.add(toy_id)
   return redirect('detail', cat_id=cat_id)
 
-
+@login_required
 def unassoc_toy(request, cat_id, toy_id):
-  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
   return redirect('detail', cat_id=cat_id)
     
+def signup(request):
+  error_message = ''
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  return render(request, 'registration/signup.html', {'form': form, 'error': error_message} )
 
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
   model = Cat
   fields = ['name', 'breed', 'description', 'age']
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
   # success_url = '/cats/'
   # Can do it this way, but not preferred way.
   
-class CatUpdate(UpdateView):
+class CatUpdate(LoginRequiredMixin, UpdateView):
   model = Cat
   fields = ['description', 'age']
   
   
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
   model = Cat
   success_url = '/cats/'
   # When deleting you use this method of redirecting because there wont be a model left it will be empty.
   
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
     
