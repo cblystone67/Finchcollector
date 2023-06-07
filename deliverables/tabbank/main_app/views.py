6ded2654a91d5c4bb662fcfe9c5bbe12f357fbba
cblystone67 <christopherblystone1@gmail.com>
2023-06-06 18:43:52 -0700

    First Api Get

--- deliverables/tabbank/main_app/views.py
+++ deliverables/tabbank/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import requests
 
 # Create your views here.
 def home(request):
@@ -7,3 +7,12 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def api_data(request):
+    url = 'https://www.songsterr.com/a/ra/songs.json?pattern=master+of+puppets'
+    
+    response = requests.get(url)
+    data = response.json()
+
+    return render(request, 'about.html', {'data': data})
+  
